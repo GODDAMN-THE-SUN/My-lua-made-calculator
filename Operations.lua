1bfb9519bca16da0733d34849b204b9139712fdc
RegentRedful <redful.jms@gmail.com>
2026-04-12 15:45:43 +0800

    refactor(operations): prevent typecheck from outputting always nil type

--- Operations.lua
+++ Operations.lua
@@ -1,11 +1,11 @@
 -- Throw an exception when one or two of the operands are not a number.
 local function typecheck(op1, op2)
     if type(op1) ~= "number" and type(op2) ~= "number" then
-        error(string.format("Both operands are not numbers! Instead got type %s and %s", type(op1), type(op2)))
+        error("Both operands are not numbers!")
     elseif type(op1) ~= "number" then
-        error(string.format("Operand 1 is not a number! Instead got type %s", type(op1)))
+        error("Operand 1 is not a number!")
     elseif type(op2) ~= "number" then
-        error(string.format("Operand 2 is not a number! Instead got type %s", type(op2)))
+        error("Operand 2 is not a number!")
     end
 end
 
